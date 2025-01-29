9fc7f761aea5cb96be3044731f2178e23cb7aff2
Timothy Williams <TimothyCalvinWilliams@gmail.com>
2025-01-29 13:44:08 +0100

    almost but it goes out of bounds

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import Carousel from "./components/Carousel";
 import ClickablePicture from "./components/ClickablePicture";
 import Counter from "./components/Counter";
 import Dice from "./components/Dice";
 import LikeButton from "./components/LikeButton";
 
 function App() {
+
+  const images=[
+    "https://randomuser.me/api/portraits/women/1.jpg",
+    "https://randomuser.me/api/portraits/men/1.jpg",
+    "https://randomuser.me/api/portraits/women/2.jpg",
+    "https://randomuser.me/api/portraits/men/2.jpg"
+  ];
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
@@ -13,6 +21,7 @@ function App() {
       <Counter></Counter>
       <ClickablePicture></ClickablePicture>
       <Dice></Dice>
+      <Carousel images = {images}></Carousel>
       
 
     </div>
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,3 +1,24 @@
 import { useState } from "react";
 
-const Carousel = []
+const Carousel = ({ images }) =>{
+    const [index,setIndex] = useState(0);
+
+    const handleNext =()=>{
+        setIndex((nextIndex) => nextIndex +1 )
+    }
+
+    const handlePrevious = () =>{
+        setIndex((nextIndex) => nextIndex -1)
+
+    }
+
+    return (
+        <div>
+            <button onClick={handlePrevious}> Previous Image </button>
+            <img src={images[index]} alt="Something went wrong" />
+            <button onClick={handleNext}> Next  Image</button>
+        </div>
+    )
+}
+
+export default Carousel
